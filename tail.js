19c23b51aea0cbb874d11d8f96a49b812a3fd633
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-04 23:28:17 +0000

    tail function

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍔 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(actual) {
+  actual.shift();
+  return actual;
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("Hi", "Hi");
+// assertEqual(6, 7);
