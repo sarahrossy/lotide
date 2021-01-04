a830f1996cf717b7f9e79fe016cb5541c1545556
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-04 23:15:01 +0000

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😀 Assertion Passed: " + actual + " === " + expected)
+    console.log(`😀 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🍔 Assertion failed: " + actual + 
-    " !== " + expected);
+    console.log(`🍔 Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
