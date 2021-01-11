99b2ec7267f2a29cffda2e533adcf8c74edb9841
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-11 18:51:52 +0000

    cleanup

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,6 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["String", "One"], ["String", "One"]), true);
