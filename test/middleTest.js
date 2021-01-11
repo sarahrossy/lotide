f59eaed07645efa537d840ac85cbb7fad70879df
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-11 20:18:22 +0000

    mocha/chai for middleTest and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,18 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+
+describe("#middle", () => {
+  it("returns [4, 9]", () => {
+    assert.deepEqual(middle([2, 4, 9, 8]), [4, 9]);
+  });
+
+  it("returns [6]", () => {
+    assert.deepEqual(middle([1, 6, 2]), [6]);
+  });
+});
+
+
 
 
 assertArraysEqual(middle([2, 4, 9, 8]), [4, 9]); // length-1 is odd
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
-const resultLength = tail(["Hello", "Lighthouse", "Labs"]).length;
-const expectedLength = ["Lighthouse", "Labs"].length
-assertEqual(resultLength, expectedLength);
+describe("#tail", () => {
+  it("returns true", () => {
+
+    const resultLength = tail(["Hello", "Lighthouse", "Labs"]).length;
+    const expectedLength = ["Lighthouse", "Labs"].length
+
+    assert.strictEqual(assertEqual(resultLength, expectedLength)
+    );
+  });
+});
