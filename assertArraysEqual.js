847faff610fb1e709d348f1346e62117d1d94180
Sarah Rossy <sarahrossysings@gmail.com>
2021-01-11 19:18:54 +0000

    refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,9 @@
-const eqArrays = function(a, b) {
-  if (a.length === b.length) {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      } 
-    }
-  } else {
-    return false;
-  }
-  return true;
-}; 
+const eqArrays = require('./eqArrays');
 
 const assertArraysEquals = function(a, b) {
   if (eqArrays(a, b)) {
-    console.log(`Assertion Passed: ${a} === ${b}`);
-  } else console.log(`Assertion Failed: ${a} !== ${b}`);
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else console.log(`❌❌❌ Assertion Failed: ${a} !== ${b}`);
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 4, 3], [1, 2, 3]));
-assertArraysEquals(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEquals(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEquals;
--- head.js
+++ head.js
@@ -1,5 +1,5 @@
-const assertEqual = require('./assertEqual')
-;
+const assertEqual = require('./assertEqual');
+
 const head = function(actual) {
   return actual[0];
 }
